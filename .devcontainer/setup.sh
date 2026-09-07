21bdb3011786ff697fadcc62de05fd5041e7e170
Mohd Maaz Khan <kmaaz7256@gmail.com>
2026-09-07 21:09:15 +0530

    test-1

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,6 +6,7 @@ set -e
 
 conda init --all
 micromamba shell init -s bash
+df
 micromamba env create -f environment.yml --yes
 # Note that `micromamba activate numpy-dev` doesn't work, it must be run by the
 # user (same applies to `conda activate`)
